3354b3f48ccc69f797ba0115a5e1fa568ec70590
LauraGA777 <laura_garciaa@soy.sena.edu.co>
2025-03-24 03:51:06 -0500

    Prueba de internet

--- src/core/database/connection.js
+++ src/core/database/connection.js
@@ -12,6 +12,7 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
         dialect: 'postgres',
+        dialectModule: pg,
         dialectOptions: {
             ssl: process.env.DB_SSL === 'true' ? {
                 require: true,
